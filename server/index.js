0a66ea3e5ee83b26e1cc6a6a785f58b9f01c46fe
Brian McCann <bmccann36@gmail.com>
2017-10-30 16:36:08 -0400

    first commit set up express routing added some dependecies

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const volleyball = require('volleyball')
+const api = require('./api')
+
+// you'll of course want static middleware so your browser can request things like your 'bundle.js'
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(volleyball)
+
+// Any routes or other various middlewares should go here!
+app.use('/api', api )
+
+// Make sure this is right at the end of your server logic!
+// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
+// (However, if you have middleware to serve up 404s, that go would before this as well)
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public'));
+});
+
+app.use( (err, req, res, next) => {
+  res.status(err.status || 500).send(err.message || 'internal server error')
+})
